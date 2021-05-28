2a272e5997e664099a1ee3e99dbf276c3f6d8b87
gabruw <gabrielmarques.mg@gmail.com>
2021-05-28 17:43:31 -0300

    feat: Correção do layout da SplashScreen e reparos

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SystemContextProvider } from 'storages/system/context';
 import Router from './routes';
-import AnimalCard from './views/animals/animals-list/animals-card/index';
-import AnimalList from './views/animals/animals-list/list-animals-card/index';
+
+//#endregion
 
 const App = () => (
     <SafeAreaProvider>
         <SystemContextProvider>
             <SplashLoader>
                 <Router />
-                {/* <AnimalList/> */}
             </SplashLoader>
         </SystemContextProvider>
     </SafeAreaProvider>
